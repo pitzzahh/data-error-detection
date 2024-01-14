97678961388fa7a366b9b5751c61cb60142f3b63
Peter John Arao <pitzzahh@gmail.com>
2024-01-14 10:38:04 +0800

    :recycle: Update Main

--- src/main/java/tech/araopj/Main.java
+++ src/main/java/tech/araopj/Main.java
@@ -1,7 +1,11 @@
 package tech.araopj;
 
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        new Random().ints(Integer.MIN_VALUE, Integer.MAX_VALUE)
+                .limit(Byte.MAX_VALUE)
+                .forEach(randomNumber -> System.out.println("randomNumber = " + randomNumber));
     }
 }
