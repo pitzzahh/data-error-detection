9482c6180f7aeb785b706d2685a873bcca19a616
Peter John Arao <pitzzahh@gmail.com>
2024-01-15 16:12:01 +0800

    :memo: Update main Parity Check sample

--- src/main/java/me/araopj/Main.java
+++ src/main/java/me/araopj/Main.java
@@ -6,7 +6,11 @@ import me.araopj.dataErrorDetection.Handler;
 public class Main {
     public static void main(String[] args) {
         Handler parityCheckHandler = new ParityCheck();
-        parityCheckHandler.handle("Hi, World!");
-        parityCheckHandler.handle("AIMHigh");
+        ParityCheck.oddValue = 0;
+        ParityCheck.evenValue = 1;
+        parityCheckHandler.handle("Hello, World!");
+        ParityCheck.oddValue = 1;
+        ParityCheck.evenValue = 0;
+        parityCheckHandler.handle("pitzzahh");
     }
 }
